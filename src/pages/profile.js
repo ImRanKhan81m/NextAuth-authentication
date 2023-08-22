d8d3fd53616e8a9995b21c92b3bc707997e4f28f
Imran Hossen <imranhossen81m@gmail.com>
2023-08-22 19:44:52 +0600

    next auth done

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,14 +1,19 @@
 import { Avatar } from "antd";
 import { UserOutlined } from "@ant-design/icons";
-
 import Head from "next/head";
+import { useSession } from "next-auth/react";
+
+
 const ProfilePage = () => {
+  const { data: session } = useSession();
+
+  console.log(session);
   return (
     <div style={{ textAlign: "center" }}>
       <Head>
         <title>Next Profile</title>
       </Head>
-      <h1>User Profile</h1>
+      <h1>{session?.user?.name}</h1>
       <Avatar size={64} icon={<UserOutlined />} />
     </div>
   );
